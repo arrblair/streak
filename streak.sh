22cf58d6b3fa899573431f63f48c8e4383a9cbd9
arrblair <ryan@livefyre.com>
2014-11-03 01:02:25 -0800

    add TODO, clean up streak, & we're LIVE

--- streak.sh
+++ streak.sh
@@ -1,18 +1,9 @@
 #!/bin/shell
 
-echo '\n' >> ~/projects/home/context/README.txt
+selected_file=`(cd commit_files/ && gshuf -n1 -e *)`
+commit_message=`head -1 commit_files/$selected_file`
+commit_body=`sed -n '2,$p' commit_files/$selected_file`
 
-
-# create way to generate a generic matching commit for each message                 
-# how about a separate little program for each of these in Python?
-## they don't have to be Python, they just need to be text appended to a file
-## that reflects generically the nature of the commit message
-
-
-selected_file=`shuf -n1 -e *`
-
-commit_message=`head -1`
-commit_body=`sed -n '2,$p' $selected_file`
 echo $commit_body > WIP.sh
 
 (cd ~/projects/home/streak && git add ~/projects/home/streak/WIP.sh)
