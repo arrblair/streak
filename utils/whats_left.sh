b0e6190f5acf5f0141a714cfcd2f331e5bec58bc
arrblair <ryan@livefyre.com>
2014-11-03 19:16:25 -0800

    change to less than

--- utils/whats_left.sh
+++ utils/whats_left.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
+
 (cd commit_files &&
 for f in *; do
     line_count=`wc -l "$f" | awk '{ print $1 }'`
-    if [ $line_count -gt 1 ]
+    if [ $line_count -lt 1 ]
     then
       echo "$f"
     fi
